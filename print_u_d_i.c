93ee62d15022db80701a85353af7c95c174fd57e
Natasha <natasha.flausino@hotmail.com>
2021-04-15 23:40:55 -0300

    newcommit

--- print_u_d_i.c
+++ print_u_d_i.c
@@ -6,7 +6,7 @@
 /*   By: nbarreir <nbarreir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 23:52:18 by csantos-          #+#    #+#             */
-/*   Updated: 2021/04/15 23:34:38 by nbarreir         ###   ########.fr       */
+/*   Updated: 2021/04/15 23:40:27 by nbarreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,36 +30,8 @@ void			print_conferir_d_i(t_flags *flags, int size)
 ** Prints decimal and int
 */
 
-static void		verify_d_i(t_flags *flags, long int num)
+static void		verify_print_d_i(t_flags *flags, char *number, int size)
 {
-	if (flags->dot == 1)
-	{
-		flags->zero = 0;
-		flags->padding = ' ';
-	}
-	if (num == INT_MIN)
-	{
-		num = 2147483648;
-		flags->negative = 1;
-	}
-	if (num < 0)
-	{
-		flags->negative = 1;
-		num = num * (-1);
-	}
-}
-
-void			print_d_i(t_flags *flags, long int num)
-{
-	int		size;
-	char	*number;
-
-	flags->count++;
-	verify_d_i(flags, num);
-	number = ft_itoa(num);
-	size = (int)ft_strlen(number);
-	if (num == 0 && flags->dot == 1)
-		size = 0;
 	if (flags->negative == 1)
 	{
 		size = size + 1;
@@ -75,6 +47,34 @@ void			print_d_i(t_flags *flags, long int num)
 	else
 		print_nat_uhex(flags, number, size);
 	free(number);
+}
+
+void			print_d_i(t_flags *flags, long int num)
+{
+	int		size;
+	char	*number;
+
+	flags->count++;
+	if (flags->dot == 1)
+	{
+		flags->zero = 0;
+		flags->padding = ' ';
+	}
+	if (num == INT_MIN)
+	{
+		num = 2147483648;
+		flags->negative = 1;
+	}
+	if (num < 0)
+	{
+		flags->negative = 1;
+		num = num * (-1);
+	}
+	number = ft_itoa(num);
+	size = (int)ft_strlen(number);
+	if (num == 0 && flags->dot == 1)
+		size = 0;
+	verify_print_d_i(flags, number, size);
 }
 
 /*
